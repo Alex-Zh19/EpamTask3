1d88151a970554f568b84127b33aa56072649504
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2021-04-12 13:50:20 +0300

    changed reexp to validate file

--- src/main/java/com/epam/task3/validator/CustomValidator.java
+++ src/main/java/com/epam/task3/validator/CustomValidator.java
@@ -3,8 +3,12 @@ package com.epam.task3.validator;
 
 public class CustomValidator {
     private static final String REG_EXP_TO_VALIDATE_STRING =
-            "^\\w+\\s{0,4}[:]\\s{0,4}[(][-]?\\s{0,4}[0-9]+\\s{0,4}(,|;)[-]?\\s{0,4}[0-9]+\\s{0,4}(,|;)[-]?\\s{0,4}[0-9]+\\s{0,4}[)](,|;)\\s{0,4}[0-9]+$";
-//сменить для даблов регэкспу
+            "^\\w+\\s{0,4}[:]\\s{0,4}\\w+\\s{0,4}[:]\\s{0,4}" +//name and type
+                    "[(]\\s{0,4}[-]?[0-9]+[.]?[0-9]+\\s{0,4}(,|;)" +//first coordinate
+                    "\\s{0,4}[-]?[0-9]+[.]?[0-9]+\\s{0,4}(,|;)" +//second coordinate
+                    "\\s{0,4}[-]?[0-9]+[.]?[0-9]+\\s{0,4}[)]" +//third coordinate
+                    "(,|;)\\s{0,4}[0-9]+[.]?[0-9]+$";//side length
+    
     public static boolean validateString(String str) {
         return str.matches(REG_EXP_TO_VALIDATE_STRING);
     }
