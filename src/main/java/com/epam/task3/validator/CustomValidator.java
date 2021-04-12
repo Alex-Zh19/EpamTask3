313614174cfb0d366cd553527d901368472c7815
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2021-04-12 17:24:52 +0300

    name changed

--- src/main/java/com/epam/task3/validator/CustomValidator.java
+++ src/main/java/com/epam/task3/validator/CustomValidator.java
@@ -2,7 +2,7 @@ package com.epam.task3.validator;
 
 
 public class CustomValidator {
-    private static final String REG_EXP_TO_VALIDATE_STRING =
+    private static final String REG_EXP_TO_VALIDATE_STRING_IN_FILE =
             "^\\w+\\s{0,4}[:]\\s{0,4}\\w+\\s{0,4}[:]\\s{0,4}" +//name and type
                     "[(]\\s{0,4}[-]?[0-9]+[.]?[0-9]+\\s{0,4}(,|;)" +//first coordinate
                     "\\s{0,4}[-]?[0-9]+[.]?[0-9]+\\s{0,4}(,|;)" +//second coordinate
@@ -10,7 +10,7 @@ public class CustomValidator {
                     "(,|;)\\s{0,4}[0-9]+[.]?[0-9]+$";//side length
 
     public static boolean validateString(String str) {
-        return str.matches(REG_EXP_TO_VALIDATE_STRING);
+        return str.matches(REG_EXP_TO_VALIDATE_STRING_IN_FILE);
     }
 
 
