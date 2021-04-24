894422a0eb682f8ad19ce2281b1c8f300c4485e9
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2021-04-25 00:23:02 +0300

    add isOnAxis

--- src/main/java/com/epam/task3/action/cube/CubeCalculation.java
+++ src/main/java/com/epam/task3/action/cube/CubeCalculation.java
@@ -1,7 +1,11 @@
 package com.epam.task3.action.cube;
 
+import com.epam.task3.action.point.CustomPointCalculation;
 import com.epam.task3.entity.Cube;
+import com.epam.task3.entity.CustomPoint;
 import com.epam.task3.exception.CubeException;
+
+import java.util.List;
 
 public class CubeCalculation {
 
@@ -21,6 +25,44 @@ public class CubeCalculation {
         checkNotNull(cube);
         double result = cube.getSideLength() * 12;
         return result;
+    }
+
+    public boolean isCubeOnTheAxis(Cube cube) throws CubeException {
+        checkNotNull(cube);
+        CustomPoint centerPoint = cube.getCenterPoint();
+        double halfSideLen = cube.getSideLength()/2;
+        double x = centerPoint.getX();
+        double y = centerPoint.getY();
+        double z = centerPoint.getZ();
+        if (x-halfSideLen==0||y-halfSideLen==0||z-halfSideLen==0){
+            return true;
+        }
+        return false;
+    }
+
+    public double volumeRatio(Cube cube) throws CubeException{
+        CustomPoint centerPoint=cube.getCenterPoint();
+        double sideLength=cube.getSideLength();
+        CustomPointCalculation pointCalculation=new CustomPointCalculation();
+        List<CustomPoint> vertexPoints=pointCalculation.findAllVertexPoints(centerPoint,sideLength);
+        if(!vertexPoints.isEmpty()){
+            CustomPoint firstDiagonalPoint=vertexPoints.get(0);
+            CustomPoint secondDiagonalPoint=vertexPoints.get(vertexPoints.size()-1);
+            double firstX=firstDiagonalPoint.getX();
+            double firstY=firstDiagonalPoint.getY();
+            double firstZ=firstDiagonalPoint.getZ();
+
+            double secondX=secondDiagonalPoint.getX();
+            double secondY=secondDiagonalPoint.getY();
+            double secondZ=secondDiagonalPoint.getZ();
+            if(firstX/secondX<0){
+                //доделать
+            }
+
+        }
+        else{
+            throw new CubeException("Wrong data input. vertex points cannot be null if center point existed");
+        }
     }
 
 
--- src/main/java/com/epam/task3/action/point/CustomPointCalculation.java
+++ src/main/java/com/epam/task3/action/point/CustomPointCalculation.java
@@ -2,6 +2,9 @@ package com.epam.task3.action.point;
 
 import com.epam.task3.entity.CustomPoint;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomPointCalculation {
     public CustomPoint findVertexPoint(CustomPoint centerPoint, double axisOffset,
                                        double axisMultiplierX, double axisMultiplierY, double axisMultiplierZ) {
@@ -20,4 +23,60 @@ public class CustomPointCalculation {
 
         return new CustomPoint(x, y, z);
     }
+
+    public List<CustomPoint> findAllVertexPoints(CustomPoint centerPoint, double sideLength) {
+        List<CustomPoint> vertexPoints = new ArrayList<>();
+        double halfSideLength = sideLength / 2;
+        // 4 points lower than center point
+        double zLow = centerPoint.getZ() - halfSideLength;
+        CustomPoint point1 = findFirstPoint(centerPoint,halfSideLength,zLow);
+        CustomPoint point2 = findSecondPoint(centerPoint,halfSideLength,zLow);
+        CustomPoint point3 = findThirdPoint(centerPoint,halfSideLength,zLow);
+        CustomPoint point4 = findFourthPoint(centerPoint,halfSideLength,zLow);
+
+        vertexPoints.add(point1);
+        vertexPoints.add(point2);
+        vertexPoints.add(point3);
+        vertexPoints.add(point4);
+
+        double zHigh = centerPoint.getZ() + halfSideLength;
+        CustomPoint point5 = findFirstPoint(centerPoint,halfSideLength,zHigh);
+        CustomPoint point6 = findSecondPoint(centerPoint,halfSideLength,zHigh);
+        CustomPoint point7 = findThirdPoint(centerPoint,halfSideLength,zHigh);
+        CustomPoint point8 = findFourthPoint(centerPoint,halfSideLength,zHigh);
+
+        //first and last points lie on the diagonal
+        vertexPoints.add(point5);
+        vertexPoints.add(point6);
+        vertexPoints.add(point7);
+        vertexPoints.add(point8);
+
+        return vertexPoints;
+    }
+
+    private CustomPoint findFirstPoint(CustomPoint centerPoint, double halfSideLength, double z) {
+        double x = centerPoint.getX() - halfSideLength;
+        double y = centerPoint.getY() - halfSideLength;
+        return new CustomPoint(x,y,z);
+    }
+
+    private CustomPoint findSecondPoint(CustomPoint centerPoint, double halfSideLength, double z) {
+        double x = centerPoint.getX() + halfSideLength;
+        double y = centerPoint.getY() - halfSideLength;
+        return new CustomPoint(x,y,z);
+    }
+
+    private CustomPoint findThirdPoint(CustomPoint centerPoint, double halfSideLength, double z) {
+        double x = centerPoint.getX() - halfSideLength;
+        double y = centerPoint.getY() + halfSideLength;
+        return new CustomPoint(x,y,z);
+    }
+
+    private CustomPoint findFourthPoint(CustomPoint centerPoint, double halfSideLength, double z) {
+        double x = centerPoint.getX() + halfSideLength;
+        double y = centerPoint.getY() + halfSideLength;
+        return new CustomPoint(x,y,z);
+    }
+
+
 }
